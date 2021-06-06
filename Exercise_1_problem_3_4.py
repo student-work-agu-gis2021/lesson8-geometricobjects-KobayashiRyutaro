76eb58f4a1c60160c305e5f94eba2050c99867b2
KobayashiRyutar <>
2021-06-06 23:31:26 +0000

    I added a code

--- Exercise_1_problem_3_4.py
+++ Exercise_1_problem_3_4.py
@@ -11,7 +11,7 @@
 # YOUR CODE HERE 1 to read the file
 #raise NotImplementedError()
 import pandas as pd 
-fp = 'data/travelTime_2015_Helsinki.txt'
+fp = 'data/travelTimes_2015_Helsinki.txt'
 data = pd.read_csv(fp,sep=';')
 
 #Check how many rows and columns there are:
